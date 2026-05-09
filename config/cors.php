3548c2c9438190e543bb92dc5228357bce3775dd
kyjita <kyjita@gmail.com>
2026-05-09 11:33:24 +0800

    Allow production frontend origins for CORS

--- config/cors.php
+++ config/cors.php
@@ -1,14 +1,35 @@
 <?php
 
+$frontendUrl = rtrim((string) env('FRONTEND_URL', ''), '/');
+$configuredOrigins = array_filter(array_map(
+    'trim',
+    explode(',', env('TRUTHSHIELD_ALLOWED_ORIGINS', '')),
+));
+$defaultOrigins = array_filter([
+    'http://127.0.0.1:15173',
+    'http://localhost:15173',
+    'https://truth-shield.otus.tw',
+    'https://truthshield.otus.tw',
+    'https://truthshield-frontend-676428765728.asia-east1.run.app',
+    $frontendUrl,
+]);
+
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => array_values(array_filter(array_map(
-        'trim',
-        explode(',', env('TRUTHSHIELD_ALLOWED_ORIGINS', 'http://127.0.0.1:15173,http://localhost:15173')),
-    ))),
+    'allowed_origins' => array_values(array_unique([...$defaultOrigins, ...$configuredOrigins])),
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    'allowed_headers' => [
+        'Accept',
+        'Accept-Language',
+        'Authorization',
+        'Content-Type',
+        'X-Requested-With',
+        'X-TruthShield-Extension-Nonce',
+        'X-TruthShield-Extension-Signature',
+        'X-TruthShield-Read-Seconds',
+        'X-TruthShield-Scroll-Depth',
+    ],
     'exposed_headers' => [],
     'max_age' => 3600,
     'supports_credentials' => false,
